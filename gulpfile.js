7dde65b18da6918045ef185900ec39214aaadedb
David Coy <david@obsidianspork.io>
2016-11-26 17:07:39 -0600

    Add babel presets for es2015

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+
+gulp.task('default', function() {
+  // Node source
+  gulp.src("es6/**/*.js")
+    .pipe(babel({presets: ["es2015"]}))
+    .pipe(gulp.dest("dist"));
+  // Browser source
+  gulp.src("public/es6/**/*.js")
+    .pipe(babel({presets: ["es2015"]}))
+    .pipe(gulp.dest("public/dist"));
+});
