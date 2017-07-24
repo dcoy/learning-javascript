a003fc29d2f1fbce2c63076349f80786919045a2
David Coy <david@obsidianspork.io>
2017-07-24 09:59:07 -0500

    Gulp tasks setup

---
+++ firstProject/public/es6/test.js
@@ -0,0 +1,15 @@
+'use scrict';
+// es6 feature: block-scoped "let" declaration
+const sentences = [
+  { subject: 'JavScript', verb: 'is', object:  'great' },
+  { subject: 'Elephants', verb: 'are', object: 'large' },
+];
+
+function say({ subject, verb, object }) {
+  // es6 feature: template strings
+  console.log(`${subject}, ${verb}, ${object}`);
+}
+// es6 feature: for..of
+for(let s of sentences) {
+  say(s);
+}
